8a28dd7b4811e2adaa1506bcc79715a623a4f5e7
onurusluca <uslucaonur@gmail.com>
2023-05-31 02:40:36 +0900

    oh boy, here we go implementing stuff again! 6.1

--- src/game/PlayerBanner.ts
+++ src/game/PlayerBanner.ts
@@ -1,7 +1,8 @@
 import Phaser from "phaser";
 
 export class PlayerBanner extends Phaser.GameObjects.Container {
-  private statusIcon: Phaser.GameObjects.Image;
+  private background: Phaser.GameObjects.Graphics;
+  private statusIcon: Phaser.GameObjects.Graphics;
   private playerName: Phaser.GameObjects.Text;
   private playerType: Phaser.GameObjects.Text;
   private playerStatus: Phaser.GameObjects.Text;
@@ -19,13 +20,20 @@ export class PlayerBanner extends Phaser.GameObjects.Container {
   ) {
     super(scene, x, y);
 
-    this.statusIcon = scene.add.image(0, 0, userStatus);
+    this.background = scene.add.graphics({ x: 0, y: 0 });
+    this.background.fillStyle(parseInt(userBannerColor.replace(/^#/, ""), 16), 1);
+    this.background.fillRect(0, 0, 220, 60);
+
+    this.statusIcon = scene.add.graphics({ x: 0, y: 0 });
+    this.statusIcon.fillStyle(this.getStatusColor(userStatus), 1);
+    this.statusIcon.fillCircle(10, 20, 10);
+
     this.playerName = scene.add.text(50, 0, userName, {
       color: userNameColor,
       fontSize: "20px",
       fontStyle: "bold",
     });
-    this.playerType = scene.add.text(200, 0, `(${userType})`, {
+    this.playerType = scene.add.text(150, 2, `(${userType})`, {
       color: "rgba(255,255,255,0.8)",
       fontSize: "15px",
     });
@@ -34,9 +42,29 @@ export class PlayerBanner extends Phaser.GameObjects.Container {
       fontSize: "15px",
     });
 
-    this.add([this.statusIcon, this.playerName, this.playerType, this.playerStatus]);
+    this.add([
+      this.background,
+      this.statusIcon,
+      this.playerName,
+      this.playerType,
+      this.playerStatus,
+    ]);
     scene.add.existing(this);
   }
+
+  private getStatusColor(status: string): number {
+    switch (status) {
+      case "online":
+        return 0x00ff00; // green
+      case "offline":
+        return 0xff0000; // red
+      case "away":
+        return 0xffff00; // yellow
+      default:
+        return 0xffffff; // white
+    }
+  }
+
   public updatePosition(x: number, y: number) {
     this.x = x;
     this.y = y;
